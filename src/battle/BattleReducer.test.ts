ca90f1fdd07768c5ffb752b0418e67c25827f267
Luke 10x <spam@luke10x.dev>
2020-07-30 11:46:00 +0100

    ✨ Do not lose health if you are not hit

---
+++ src/battle/BattleReducer.test.ts
@@ -0,0 +1,69 @@
+import { battleReducer, BattleAction } from './BattleReducer';
+
+describe('Each roll may cost some health', () => {
+  describe('When they roll equal', () => {
+    const diceRolledAction: BattleAction = {
+      actionType: 'DiceRolled',
+      player1: 6,
+      player2: 5,
+      monster1: 3,
+      monster2: 2,
+    };
+    const stateAfterEqualRoll = battleReducer(undefined, diceRolledAction);
+
+    test('player health stays the same', () => {
+      expect(stateAfterEqualRoll.player).toEqual({ health: 100, lastHit: 0 });
+    });
+
+    test('monster health stays the same', () => {
+      expect(stateAfterEqualRoll.player).toEqual({ health: 100, lastHit: 0 });
+    });
+  });
+
+  describe('When player rolls higher than monster', () => {
+    const diceRolledAction: BattleAction = {
+      actionType: 'DiceRolled',
+      player1: 6,
+      player2: 5,
+      monster1: 3,
+      monster2: 2,
+    };
+    const stateAfterFirstRoll = battleReducer(undefined, diceRolledAction);
+
+    test('player health stays the same', () => {
+      expect(stateAfterFirstRoll.player).toEqual({ health: 100, lastHit: 0 });
+    });
+    test.todo('monster health is hit');
+    describe('When player rolls higher than monster again (this time much higher)', () => {
+      const diceRolledAction: BattleAction = {
+        actionType: 'DiceRolled',
+        player1: 6,
+        player2: 6,
+        monster1: 1,
+        monster2: 1,
+      };
+      const stateAfterSecondRoll = battleReducer(stateAfterFirstRoll, diceRolledAction);
+
+      test('player health stays the same still', () => {
+        expect(stateAfterSecondRoll.player).toEqual({ health: 100, lastHit: 0 });
+      });
+      test.todo('monster health is hit even more');
+    });
+    describe('Revenge: When monster rolls higher than player', () => {
+      const diceRolledAction: BattleAction = {
+        actionType: 'DiceRolled',
+        player1: 1,
+        player2: 1,
+        monster1: 3,
+        monster2: 2,
+      };
+      const stateAfterSecondRoll = battleReducer(stateAfterFirstRoll, diceRolledAction);
+
+      test.todo('player health is hit this time');
+
+      test('monster health stays the same, since it was hit last time', () => {
+        expect(stateAfterSecondRoll.player).toEqual({ health: 100, lastHit: 0 });
+      });
+    });
+  });
+});
---
+++ src/battle/BattleReducer.ts
@@ -0,0 +1,38 @@
+interface Person {
+  health: number;
+  lastHit: number;
+}
+
+interface Battle {
+  player: Person;
+  monster: Person;
+  battleInProgress: boolean;
+}
+
+type DiceRoll = 1 | 2 | 3 | 4 | 5 | 6;
+
+export type BattleAction =
+  | {
+      actionType: 'DiceRolled';
+      player1: DiceRoll;
+      player2: DiceRoll;
+      monster1: DiceRoll;
+      monster2: DiceRoll;
+    }
+  | { actionType: 'Reset' };
+
+const ensureOldState = (optionalOldState: Battle | undefined) => {
+  return optionalOldState === undefined
+    ? {
+        player: { health: 100, lastHit: 0 },
+        monster: { health: 100, lastHit: 0 },
+        battleInProgress: true,
+      }
+    : optionalOldState;
+};
+
+export const battleReducer = (optionalOldState: Battle | undefined, action: BattleAction): Battle => {
+  const oldState = ensureOldState(optionalOldState);
+  action; // TODO use me
+  return oldState;
+};
