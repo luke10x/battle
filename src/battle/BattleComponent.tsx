4f46c615e36a24466bf518bdee34f1426c36a8f7
Luke 10x <spam@luke10x.dev>
2020-09-03 17:25:41 +0300

    💄 Styles simplified

--- src/battle/BattleComponent.tsx
+++ src/battle/BattleComponent.tsx
@@ -8,89 +8,71 @@ import { DamageComponent } from './DamageComponent';
 
 const breakpointSmall = '620px';
 const Wrapper = styled.div`
-  font-size: 2em;
+  font-size: 1.5em;
   height: 100%;
-
   display: flex;
   flex-direction: column;
-
   .content {
-    flex: 1 1;
-
+    flex: 1;
     display: flex;
     justify-content: space-around;
     @media (max-width: ${breakpointSmall}) {
       flex-direction: column;
       justify-content: flex-start;
     }
-
     .player {
-      flex: 0 1 250px;
-      margin: 10pt;
-
+      flex: 0 0 7em;
+      margin: 0.5em;
       display: flex;
       flex-direction: column;
       @media (max-width: ${breakpointSmall}) {
         flex: 0 1;
         border: 5px dotted black;
-
         flex-direction: row;
       }
-      .nameCard {
+      .details {
         border: 5px dotted black;
         text-align: left;
-        padding: 20pt;
-
+        padding: 1em;
         @media (max-width: ${breakpointSmall}) {
           flex: 1 0;
-
           border: 0;
         }
       }
       .dice {
-        text-align: center;
-        font-size: 3em;
-
+        font-size: 2em;
         display: flex;
         justify-content: space-around;
         @media (max-width: ${breakpointSmall}) {
-          flex: 0 0 50pt;
-
-          font-size: 2em;
+          flex: 0 0 1.5em;
           line-height: 0.5em;
           flex-direction: column;
         }
       }
     }
   }
-  .won {
-    color: green;
-  }
-  .lost {
-    color: red;
-  }
   .footer {
-    flex: 0 1;
-    display: flex;
-    flex-direction: column;
-
-    button,
-    .banner {
-      flex: 1 1 100%;
-
-      margin: 10pt;
-      padding: 20pt;
-      font-size: 1em;
+    flex: 0;
+    padding: 0.5em;
+    & > * {
+      padding: 1em;
+      margin: 0px auto;
+      box-sizing: border-box;
+      width: 50%;
+      @media (max-width: ${breakpointSmall}) {
+        width: 100%;
+      }
     }
     button {
+      font-size: 1em;
       border: 5px solid black;
       background: pink;
     }
-    .banner {
-      font-size: 2em;
-      @media (max-width: ${breakpointSmall}) {
-        font-size: 1em;
-      }
+    .banner.won {
+      color: green;
+    }
+    .banner.lost {
+      color: red;
     }
   }
 `;
@@ -122,50 +104,48 @@ export const BattleComponent: React.FC = () => {
     dispatch({ actionType: 'Reset' });
   };
 
-  const rollButton = state?.battleInProgress && !rolling;
-  const resetButton = !state?.battleInProgress;
+  const rollButton = state.battleInProgress && !rolling;
+  const resetButton = !state.battleInProgress;
   const won = state.monster.health === 0;
   const lost = state.human.health === 0;
   return (
     <Wrapper>
-      {state && (
-        <div className="content">
-          <div className="player">
-            <div className="nameCard">
-              <div>HUMAN</div>
-              <div>
-                <span role="img" aria-label="health">
-                  🖤
-                </span>{' '}
-                {state.human.health} <DamageComponent value={state.human.lastHit} rolling={rolling} />
-              </div>
-            </div>
-            <div className="dice">
-              <DiceComponent rolling={rolling} lastRolled={state.human.dice1} />
-              <DiceComponent rolling={rolling} lastRolled={state.human.dice2} />
+      <div className="content">
+        <div className="player">
+          <div className="details">
+            <div>HUMAN</div>
+            <div>
+              <span role="img" aria-label="health">
+                🖤
+              </span>{' '}
+              {state.human.health} <DamageComponent value={state.human.lastHit} rolling={rolling} />
             </div>
           </div>
-          <div className="player">
-            <div className="nameCard">
-              <div>MONSTER</div>
-              <div>
-                <span role="img" aria-label="health">
-                  🖤
-                </span>{' '}
-                {state.monster.health} <DamageComponent value={state.monster.lastHit} rolling={rolling} />
-              </div>
-            </div>
-            <div className="dice">
-              <DiceComponent rolling={rolling} lastRolled={state.monster.dice1} />
-              <DiceComponent rolling={rolling} lastRolled={state.monster.dice2} />
-            </div>
+          <div className="dice">
+            <DiceComponent rolling={rolling} lastRolled={state.human.dice1} />
+            <DiceComponent rolling={rolling} lastRolled={state.human.dice2} />
           </div>
         </div>
-      )}
+        <div className="player">
+          <div className="details">
+            <div>MONSTER</div>
+            <div>
+              <span role="img" aria-label="health">
+                🖤
+              </span>{' '}
+              {state.monster.health} <DamageComponent value={state.monster.lastHit} rolling={rolling} />
+            </div>
+          </div>
+          <div className="dice">
+            <DiceComponent rolling={rolling} lastRolled={state.monster.dice1} />
+            <DiceComponent rolling={rolling} lastRolled={state.monster.dice2} />
+          </div>
+        </div>
+      </div>
       <div className="footer">
         {resetButton && <button onClick={handleReset}>Reset</button>}
-        {won && <div className="banner won">PLAYER WON</div>}
-        {lost && <div className="banner lost">PLAYER LOST</div>}
+        {won && <div className="banner won">HUMAN WON</div>}
+        {lost && <div className="banner lost">MONSTER WON</div>}
         {rollButton && <button onClick={handleRoll}>Roll!</button>}
       </div>
     </Wrapper>
