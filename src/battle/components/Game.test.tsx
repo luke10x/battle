68fe6f1343c2a95c5182960e3584180b5030c78f
Luke 10x <spam@luke10x.dev>
2020-09-11 22:46:17 +0300

    ✅ Game compoents tests its callbacks

--- src/battle/components/Game.test.tsx
+++ src/battle/components/Game.test.tsx
@@ -9,9 +9,10 @@ jest.mock('../hooks/UseBattleReducer');
 import { Board } from './Board';
 jest.mock('./Board');
 
-describe('board & battle state', () => {
-  (Board as jest.Mock).mockReturnValue(<>BOARD</>);
+import { generateRandom } from '../utils/GenerateRandom';
+jest.mock('../utils/GenerateRandom');
 
+describe('battle states', () => {
   const initialBattle: Battle = {
     monster: { health: 10, lastHit: 0 },
     human: { health: 10, lastHit: 0 },
@@ -24,24 +25,96 @@ describe('board & battle state', () => {
     inProgress: true,
   };
 
-  test.each([
-    ['initial', initialBattle],
-    ['specific', specificState],
-  ])(
-    'board is rendered with %p battle state',
-    (_testCase: string, battle: Battle) => {
-      (useBattleReducer as jest.Mock).mockReturnValueOnce([battle]);
+  describe('board just returns DOM', () => {
+    beforeEach(() => {
+      (Board as jest.Mock).mockReturnValue(<>BOARD</>);
+    });
+    afterEach(() => jest.resetAllMocks());
+
+    test.each([
+      ['initial', initialBattle],
+      ['specific', specificState],
+    ])(
+      'board is rendered with %p battle state',
+      (_testCase: string, battle: Battle) => {
+        (useBattleReducer as jest.Mock).mockReturnValueOnce([battle]);
+
+        render(<Game />);
+
+        expect(Board).toBeCalledWith(
+          {
+            battle: battle,
+            onRoll: expect.anything(),
+            onReset: expect.anything(),
+          },
+          expect.anything(),
+        );
+      },
+    );
+
+    test('no action is dispatched', () => {
+      const dispatch = jest.fn();
+      (useBattleReducer as jest.Mock).mockReturnValueOnce([
+        initialBattle,
+        dispatch,
+      ]);
 
       render(<Game />);
+      expect(dispatch).not.toHaveBeenCalled();
+    });
+  });
 
-      expect(Board).toBeCalledWith(
-        {
-          battle: battle,
-          onRoll: expect.anything(),
-          onReset: expect.anything(),
-        },
-        expect.anything(),
-      );
-    },
-  );
+  describe('board triggers its onReset property', () => {
+    beforeEach(() => {
+      (Board as jest.Mock).mockImplementation((props) => {
+        const { onReset } = props;
+        onReset();
+        return <>BOARD</>;
+      });
+    });
+    afterEach(() => jest.resetAllMocks());
+
+    test('dispatches "Reset" action', () => {
+      const dispatch = jest.fn();
+      (useBattleReducer as jest.Mock).mockReturnValueOnce([
+        initialBattle,
+        dispatch,
+      ]);
+
+      render(<Game />);
+      expect(dispatch).toHaveBeenCalledWith({ actionType: 'Reset' });
+    });
+  });
+
+  describe('board triggers its onRoll property', () => {
+    beforeEach(() => {
+      (Board as jest.Mock).mockImplementation((props) => {
+        const { onRoll } = props;
+        onRoll();
+        return <>BOARD</>;
+      });
+    });
+    afterEach(() => jest.resetAllMocks());
+
+    test('dispatches "DiceRolled" action', () => {
+      const dispatch = jest.fn();
+      (useBattleReducer as jest.Mock).mockReturnValueOnce([
+        initialBattle,
+        dispatch,
+      ]);
+
+      (generateRandom as jest.Mock)
+        .mockReturnValueOnce(1)
+        .mockReturnValueOnce(2)
+        .mockReturnValueOnce(3)
+        .mockReturnValueOnce(4);
+
+      render(<Game />);
+      expect(dispatch).toHaveBeenCalledWith({
+        actionType: 'DiceRolled',
+        human: [1, 2],
+        monster: [3, 4],
+      });
+    });
+  });
 });
